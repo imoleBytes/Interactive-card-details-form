29f5c0bbd28b638231d15e4ca635015a6eb16544
imole <dremkay71@gmail.com>
2023-01-25 13:11:52 +0100

    app.js created and all elements selected

---
+++ app.js
@@ -0,0 +1,18 @@
+const nameInput = document.querySelector('#holder-name');
+const numberInput = document.querySelector('#holder-number');
+const cvcInput = document.querySelector('#cvc');
+const monthInput = document.querySelector('#mm');
+const yearInput = document.querySelector('#yy');
+
+const cardNumber = document.querySelector('.number');
+const cardName = document.querySelector('.name');
+const cardCVC = document.querySelector('.cvc-number');
+const cardmonth = document.querySelector('#month');
+const cardYear = document.querySelector('#year');
+
+
+
+
+
+
+
